b3aa2500641ca1d243784c480ef49ff59fff7d4b
shahrouz <salimoha@ucsd.edu>
2017-01-04 16:26:44 -0800

    new pycharm

--- arya.py
+++ arya.py
@@ -1,5 +1,14 @@
 import numpy as np
+
+np.set_printoptions(linewidth=200, precision=5, suppress=True)
+import pandas as pd;
+
+pd.options.display.max_rows = 20;
+pd.options.display.expand_frame_repr = False
+import pylab as plt
 import numpy.matlib
+
+
 def bounds(bnd1,bnd2,n):
 #   find vertex of domain for a box domain.
 #   INPUT: n: dimension, bnd1: lower bound, bnd2: upper bound.
@@ -16,10 +25,13 @@ def bounds(bnd1,bnd2,n):
 	for ii in range(n):
 	    tt = np.mod(np.arange(2**n)+1, 2**(n-ii)) <= 2**(n-ii-1)-1
 	    bnds[ii, tt] = bnd1[ii];
-	return bnds
+	return pd.DataFrame(bnds)
 #TEST
-n = 3
-bnd1 = np.zeros((n, 1))
-bnd2 = np.ones((n, 1)) * 3
-bnds = bounds(bnd1,bnd2,n)
+n = 10
+bnd1 = pd.DataFrame(np.zeros((n)))
+bnd2 = pd.DataFrame(np.ones((n)) * 3)
+bnd1.values
+bnd2.values
+
+bnds = bounds(bnd1.values,bnd2.values,n)
 print(bnds)
