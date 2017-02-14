34b793e1851dce64408b004aba82ad9195cfae42
shahrouz <salimoha@ucsd.edu>
2017-02-14 09:31:22 -0800

    Alpha DOGS scratch

--- DOGS_noisy_main.py
+++ DOGS_noisy_main.py
@@ -9,18 +9,99 @@ pd.options.display.expand_frame_repr = False
 
 import pylab as plt
 
+def interpolate_val(x, inter_par):
+    if inter_par.method == "NPS":
+        w = inter_par.w
+        v = inter_par.v
+        xi = inter_par.xi
+        # Muhan modified on 2/10:
+        # Usually x is a column extracted from xi, then x becomes a one-dimension vector, when xi is two-dimension matrix.
+        # We need to convert x to be a two dimension vector. So I reshape x.
+        n = xi.shape[0]  # Row of xi
+        x = np.copy(x.reshape(n, 1))  # Similar to add a newaxis to x
+        S = xi - x
+        #             print np.dot(v.T,np.concatenate([np.ones((1,1)),x],axis=0)) + np.dot(w.T,np.sqrt(np.diag(np.dot(S.T,S))))**3
+        return np.dot(v.T, np.concatenate([np.ones((1, 1)), x], axis=0)) + np.dot(w.T, (np.sqrt(np.diag(np.dot(S.T, S))) ** 3))
+
+
+
+def regressionparametarization(xi, yi, sigma, inter_par):
+    # Notice xi, yi and sigma must be a two dimension matrix, even if you want it to be a vector.
+    # or there will be error
+    n = xi.shape[0]
+    N = xi.shape[1]
+    if inter_par.method == 'NPS':
+        A = np.zeros(shape=(N, N))
+        for ii in range(0, N, 1):  # for ii =0 to m-1 with step 1; range(1,N,1)
+            for jj in range(0, N, 1):
+                A[ii, jj] = (np.dot(xi[:, ii] - xi[:, jj], xi[:, ii] - xi[:, jj])) ** (3.0 / 2.0)
+        V = np.concatenate((np.ones((1, N)), xi), axis=0)
+        w1 = np.linalg.lstsq((np.dot(np.diag(np.divide(1, sigma[0])), V.T)), np.divide(yi, sigma).T)
+        w1 = np.copy(w1[0])
+        b = np.mean(np.divide(np.dot(V.T, w1) - yi.reshape(-1, 1), sigma) ** 2)
+        wv = np.zeros([N + n + 1])
+        if b < 1:
+            wv[N:] = np.copy(w1.T)
+            rho = 1000
+            wv = np.copy(wv.reshape(-1, 1))
+        else:
+            rho = 1.1
+            fun = lambda rho: smoothing_polyharmonic(rho, A, V, sigma, yi, n, N, 1)
+            sol = optimize.fsolve(fun, rho)
+            b, db, wv = smoothing_polyharmonic(sol, A, V, sigma, yi, n, N, 3)
+        inter_par.w = wv[:N]
+        inter_par.v = wv[N:]
+        inter_par.xi = xi
+        yp = np.zeros([N])
+        while (1):
+            for ii in range(N):
+                yp[ii] = interpolate_val(xi[:, ii], inter_par)
+            residual = np.max(np.divide(np.abs(yp - yi), sigma[0]))
+            if residual < 2:
+                break
+            rho *= 0.9
+            b, db, wv = smoothing_polyharmonic(rho, A, V, sigma, yi, n, N)
+            inter_par.w = wv[:N]
+            inter_par.v = wv[N:]
+    return inter_par, yp
+
+
+def smoothing_polyharmonic(rho, A, V, sigma, yi, n, N, num_arg):
+    # Notice: num_arg = 1 will return b
+    #         num_arg = else will return b,db,wv
+    A01 = np.concatenate((A + rho * np.diag(sigma ** 2), np.transpose(V)), axis=1)
+    A02 = np.concatenate((V, np.zeros(shape=(n + 1, n + 1))), axis=1)
+    A1 = np.concatenate((A01, A02), axis=0)
+    b1 = np.concatenate([yi.reshape(-1, 1), np.zeros(shape=(n + 1, 1))])
+    wv = np.linalg.solve(A1, b1)
+    b = np.mean(np.multiply(wv[:N], sigma) ** 2 * rho ** 2) - 1
+    bdwv = np.concatenate([np.multiply(wv[:N], sigma.reshape(-1, 1) ** 2), np.zeros((n + 1, 1))])
+    Dwv = np.linalg.solve(-A1, bdwv)
+    db = 2 * np.mean(np.multiply(wv[:N] ** 2 * rho + rho ** 2 * np.multiply(wv[:N], Dwv[:N]), sigma ** 2))
+    if num_arg == 1:
+        return b
+    else:
+        return b, db, wv
 
 
 # This script shows the alpha DOGS main code
 
 # dimenstion is n
-n = 1
+n = 2
 # the noise level
 sigma0 = 0.3
 
+def fun(x, alpha=0.1):
+    y = np.array((x[0, :] - 0.45) ** 2.0 ) +np.array((x[1, :] - 0.45) ** 2.0 )  + alpha*np.random.rand()
+    return y.T
+
+def funr(x, alpha=0.1):
+    y = np.array((x[0, :] - 0.45) ** 2.0 ) +np.array((x[1, :] - 0.45) ** 2.0 )
+    return y.T
+
 # truth function
-funr = lambda x: (5*norm(x-0.3)**2)
-fun = lambda x: (5*norm(x-0.3)**2+sigma0*np.random.rand())
+# funr = lambda x: (5*np.norm(x-0.3)**2)
+# fun = lambda x: (5*np.norm(x-0.3)**2+sigma0*np.random.rand())
 lb = 0 * np.ones((n,1))
 ub = np.ones((n,1))
 
@@ -52,21 +133,29 @@ for ff in range(nff):
     yr = np.zeros(xE.shape[1])
     T = np.zeros(xE.shape[1])
     for ii in range(xE.shape[1]):
-        yE[ii] = fun(lb+(ub-lb)*xE[:,ii])
-        yr[ii] = funr(lb+(ub-lb)*xE[:,ii])
+        # TODO: fix matrix
+        # yE[ii] = fun(lb+(ub-lb)*xE[:,ii].T)
+        # yr[ii] = funr(lb+(ub-lb)*xE[:,ii].T)
         T[ii] = 1
 
+    yE = fun(xE)
+    yi = yi[np.newaxis, :]
+    yr = funr(xE)
+    yr = yr[np.newaxis, :]
+    T = np.ones((1,len(yE)))
+
+
     SigmaT = sigma0 / np.square(T)
-    xU = Utils.bounds(np.zeros(n,1),np.ones(n,1),n)
+    # Muhan Modiefied : I suppose that here we should generate a n by 1 vector full of zeros
+    xU = Utils.bounds(np.zeros([n,1]),np.ones([n,1]),n)
 
     # initialize Nm, L, K
     L=L0
-
+    inter_par = Utils.Inter_par(method="NPS")
     for k in range(iter_max):
-        [inter_par, yp] = Utils.regressionparametarization(xE, yE, sigma0/ np.square(T), inter_method)
+        [inter_par, yp] = regressionparametarization(xE, yE, sigma0/ np.square(T), inter_par)
 
         K0 = np.ptp(yE, axis=0)
-
         #Calculate the discrete function.
         tmp = yp+SigmaT
         yt = np.amin(tmp, 0)
@@ -85,25 +174,26 @@ for ff in range(nff):
             yE[ind_exist] = ((fun(xd)) + yE(ind_exist) * T(ind_exist)) / (T(ind_exist)+1)
             T[ind_exist] = T(ind_exist)+1
         else:
-            tmp1 = sigma0 / sd.square(T(ind_exist))
-            tmp2 = 0.01 * range(yE) * (max(ub - lb)) / Nm
+            tmp1 = np.divide(sigma0 , np.sqrt(T[ind_exist]))
+            tmp2 = 0.01 * np.ptp(yE, axis=0) * (max(ub - lb)) / Nm
             if tmp1 < tmp2:
                 yd = np.inf
 
             # Calcuate the unevaluated function:
-            yu = np.empty( shape=(0, 0) ) #create empty array
+            yu = np.empty( shape=[0, 0] ) #create empty array
             if (xU.shape[1]!=0):
                 yu = np.zeros(xU.shape[1]);
                 for ii in range(xU.shape[1]):
                     tmp = Utils.interpolate_val(xU[:, ii], inter_par)-np.amin(yp,0)
-                    yu[ii] = tmp / Utils.mindis(xU[:, ii], xE)
+                    t1,_,_ = Utils.mindis(np.array([xU[:, ii]]), xE)
+                    yu[ii] = tmp / t1
 
-            if (xU.shape[1]!=0 & np.amin(yu,0) < 0):
+            if xU.shape[1]!=0 and np.min(yu) < 0:
                 t = np.amin(yu, 0)
                 ind = np.argmin(yu, 0)
                 xc = xU[:, ind]
                 yc = -np.inf
-                xU[:, ind] = np.empty( shape=(0, 0) ) #create empty array
+                xU[:, ind] = np.empty( shape=[1] ) #create empty array
             else:
                 #Minimize s_c ^ k(x)
                 while 1:
@@ -132,27 +222,28 @@ for ff in range(nff):
                         xU[:, ind] = np.empty(shape=(0, 0))  # create empty array
 
             # Minimize S_d ^ k(x)
-            if (Utils.mindis(xc, xE) < 1e-6):
+            t1,_,_ = Utils.mindis(np.array([xc]), xE)
+            if t1 < 1e-6:
                 K = 2 * K
                 Nm = 2 * Nm
                 L = L + L0
 
             if (yc < yd):
                 # keyboard
-                if (Utils.mindis(xc, xE) > 1e-6):
-                    xE = [xE, xc]
-                    yE = [yE, fun(lb + (ub - lb) * xc)]
+                t1,_,_ = Utils.mindis(np.array([xc]), xE)
+                if t1 > 1e-6:
+                    xE = np.concatenate([xE, np.array([xc])],axis=1)
+                    yE = np.concatenate((yE, np.array([fun(lb + (ub - lb) * xc)])))
                     T = [T, 1]
-                    yr = [yr, funr(lb + (ub - lb) * xc)]
+                    yr = np.concatenate([yr, np.array([funr(lb + (ub - lb) * xc)])],axis=0)
                     SigmaT = sigma0 / np.square(T)
                 else:
                     yE[ind_exist] = ((fun(lb + (ub - lb) * xd)) + yE[ind_exist] * T[ind_exist]) / (T[ind_exist] + 1)
                     T[ind_exist] = T[ind_exist] + 1
                     SigmaT = sigma0 / np.square(T)
-
-        regret[ff, k] = np.amin(yr, 0)
+        regret[ff, k] = np.min(yr)
         estimate[ff, k] = yE[ind_out]
-        datalength[ff, k] = np.shape(xE)
+        datalength[ff, k] = np.shape(xE)[1]
         mesh[ff, k] = Nm
 
 
@@ -170,4 +261,29 @@ for ff in range(nff):
 #                    such that          L * sigma(h,T)   <   eps
 #                       where eps = min { (sd_i - sc),  (sd_i - sd{i-1}) }   [with minimum N value]
 #           Improve the existing point accuracy with the N_new and T_new.
+#%%
 
+import matplotlib.pyplot as plt
+
+
+
+xx = np.arange(0, 1.05, 0.05)
+xx = pd.DataFrame(xx).values
+yy= np.zeros((len(xx),1))
+yr= np.zeros((len(xx),1))
+
+for ii in range(len(xx)):
+    yy[ii] = interpolate_val( xx[ii],inter_par)
+yr = funr(xx.T)
+# ground truth
+# yr =
+yr = yr[:,np.newaxis]
+
+plt.figure()
+plt.plot(xx, yy, '--')
+plt.plot(xx, yr, '-')
+plt.plot(xi, yi, '*')
+# example variable error bar values
+plt.errorbar(xi[0], yi[0], yerr=sigma[0], linestyle="None")
+plt.title("modeling the error function")
+plt.show()
--- tringulation_search_bound_constantK.py
+++ tringulation_search_bound_constantK.py
@@ -8,7 +8,9 @@ import pandas as pd
 import numpy as np
 import math
 from scipy.spatial import Delaunay
-#%%
+
+
+# %%
 def circhyp(x, N):
     # circhyp     Circumhypersphere of simplex
     #   [xC, R2] = circhyp(x, N) calculates the coordinates of the circumcenter
@@ -36,76 +38,81 @@ def circhyp(x, N):
     #	print(R2)
     return R2, xC
 
-def tringulation_search_bound_constantK(inter_par,xi,K,ind_min):
+
+def tringulation_search_bound_constantK(inter_par, xi, K, ind_min):
     n = xi.shape[0]
     # [R2, xC] = utl.circhyp(xi[:, tri.simplices[ind, :]], n)
     tri = Delaunay(xi.T)  # fix for 1D
     Sc = np.zeros([np.shape(tri.simplices)[0]])
     Scl = np.zeros([np.shape(tri.simplices)[0]])
     for ii in range(np.shape(tri.simplices)[0]):
-        R2, xc = circhyp(xi[:,tri.simplices[ii,:]],n)
-        x = np.dot(xi[:,tri.simplices[ii,:]] , np.ones([n+1,1])/(n+1))
-        Sc[ii] = interpolate_val(x,inter_par) - K * (R2 - np.linalg.norm(x-xc)**2)
-        if np.sum(ind_min == tri.simplices[ii,:]):
+        R2, xc = circhyp(xi[:, tri.simplices[ii, :]], n)
+        x = np.dot(xi[:, tri.simplices[ii, :]], np.ones([n + 1, 1]) / (n + 1))
+        Sc[ii] = interpolate_val(x, inter_par) - K * (R2 - np.linalg.norm(x - xc) ** 2)
+        if np.sum(ind_min == tri.simplices[ii, :]):
             Scl[ii] = Sc[ii]
         else:
             Scl[ii] = np.inf
     # Global one    
     t = np.min(Sc)
     ind = np.argmin(Sc)
-    R2, xc = circhyp(xi[:,tri.simplices[ind,:]],n)
-    x = np.dot(xi[:,tri.simplices[ind,:]] , np.ones([n+1,1])/(n+1))
-    xm,ym = Constant_K_Search(x,inter_par,xc,R2,K)
+    R2, xc = circhyp(xi[:, tri.simplices[ind, :]], n)
+    x = np.dot(xi[:, tri.simplices[ind, :]], np.ones([n + 1, 1]) / (n + 1))
+    xm, ym = Constant_K_Search(x, inter_par, xc, R2, K)
     # Local one
     t = np.min(Sc)
     ind = np.argmin(Sc)
-    R2,xc = circhyp(xi[:,tri.simplices[ind,:]],n)
+    R2, xc = circhyp(xi[:, tri.simplices[ind, :]], n)
     # Notice!! ind_min may have a problen as an index
-    x = np.copy(xi[:,ind_min-1])
-    xml,yml = Constant_K_Search(x,inter_par,xc,R2,K)
+    x = np.copy(xi[:, ind_min - 1])
+    xml, yml = Constant_K_Search(x, inter_par, xc, R2, K)
     if yml < ym:
         xm = np.copy(xml)
         ym = np.copy(yml)
-    return xm.reshape(-1,1) ,ym
-        
-def Constant_K_Search(x0,inter_par,xc,R2,K,lb=[],ub=[]):
-#    This funciron minimizes the search funciton in the specified simplex with xc as circumcenter of that simplex and R2 as the circumradius of that simplex
-#   the search funciton is: s(x) = p(x) - K e(x)
-#   where the p(x) is the surrogate model: usually polyharmonic spline (RBF) phi = r^3
-#   the artificali uncertatintiy fucniton is：e(x) = R2-norm(x-xc)
-#   K: is a constant paramtere that specifies a tradeoff bwtween gloabl exploration (e - K large) and local refinemnet (p - K small) 
-#   K is dependant on the mesh size. Its changes is proporstional  to the inverse of the rate as mesh size. 
-#    Initially the algorithm tends to explore globally. and as the algorithm procceeds it becomes dense at the position of a global minimizer.
-#     global lb,ub
-#     costfun,costjac = lambda x:Contious_search_cost(x,inter_par,xc,R2,K)
+    return xm.reshape(-1, 1), ym
+
+
+def Constant_K_Search(x0, inter_par, xc, R2, K, lb=[], ub=[]):
+    #    This funciron minimizes the search funciton in the specified simplex with xc as circumcenter of that simplex and R2 as the circumradius of that simplex
+    #   the search funciton is: s(x) = p(x) - K e(x)
+    #   where the p(x) is the surrogate model: usually polyharmonic spline (RBF) phi = r^3
+    #   the artificali uncertatintiy fucniton is：e(x) = R2-norm(x-xc)
+    #   K: is a constant paramtere that specifies a tradeoff bwtween gloabl exploration (e - K large) and local refinemnet (p - K small)
+    #   K is dependant on the mesh size. Its changes is proporstional  to the inverse of the rate as mesh size.
+    #    Initially the algorithm tends to explore globally. and as the algorithm procceeds it becomes dense at the position of a global minimizer.
+    #     global lb,ub
+    #     costfun,costjac = lambda x:Contious_search_cost(x,inter_par,xc,R2,K)
     n = x0.shape[0]
     costfun = lambda x: Contious_search_cost(x, inter_par, xc, R2, K)
     costjac = lambda x: Contious_search_cost_grad(x, inter_par, xc, R2, K)
-    opt = {'disp':True}
+    opt = {'disp': True}
     # TODO: boundas 0 to 1 all dimetnsions.. fix with lb and ub
-    bnds = tuple([ (0,1) for i in range(int(n))])
-    x00 =x0
+    bnds = tuple([(0, 1) for i in range(int(n))])
+    x00 = x0
     x0 = pd.DataFrame(x00).values
     # TODO: the output of minimize fucntion is np array (n,). For interpolte_val the input is (n,1)
     # TODO:  S=xi-x has problem in side this function
     # TODO: fix the input information for jacobi!!!!!!!!!!
-    res = optimize.minimize(costfun,x0,method='L-BFGS-B',bounds=bnds,options=opt)
+    res = optimize.minimize(costfun, x0, method='L-BFGS-B', bounds=bnds, options=opt)
     x = res.x
     y = res.fun
-    return x,y
+    return x, y
 
 
-#gradient of soncstant K search
-def Contious_search_cost_grad(x,inter_par,xc,R2,K):
-    DM = interpolate_grad(x,inter_par).reshape(-1,1) + 2*K*(x-xc)
+# gradient of soncstant K search
+def Contious_search_cost_grad(x, inter_par, xc, R2, K):
+    DM = interpolate_grad(x, inter_par).reshape(-1, 1) + 2 * K * (x - xc)
     dm = pd.DataFrame(DM)
     return DM.T
     # return dm.values
 
+
 # value of consatn K search
-def Contious_search_cost(x,inter_par,xc,R2,K):
-    M = interpolate_val(x,inter_par) - K*(R2 - np.linalg.norm(x-xc)**2)
+def Contious_search_cost(x, inter_par, xc, R2, K):
+    M = interpolate_val(x, inter_par) - K * (R2 - np.linalg.norm(x - xc) ** 2)
     return M
+
+
 # Muhan-->implementaiton #TODO
 # def Contious_search_cost(x,inter_par,xc,R2,K):
 #     M = interpolate_val(x,inter_par) - K*(R2 - np.linalg.norm(x-xc)**2)
@@ -116,7 +123,7 @@ def Contious_search_cost(x,inter_par,xc,R2,K):
 #     return M
 # #%%
 
-#%%
+# %%
 def interpolate_val(x, inter_par):
     if inter_par.method == "NPS":
         w = inter_par.w
@@ -124,11 +131,15 @@ def interpolate_val(x, inter_par):
         xi = inter_par.xi
         try:
             S = xi - x
-            return np.dot(v.T, np.concatenate([np.ones((1, 1)), x], axis=0)) + np.dot(w.T, ( np.sqrt(np.diag(np.dot(S.T, S))) ** 3))
+            return np.dot(v.T, np.concatenate([np.ones((1, 1)), x], axis=0)) + np.dot(w.T, (
+            np.sqrt(np.diag(np.dot(S.T, S))) ** 3))
         except:
-            S =  xi - np.tile(x.reshape(-1,1),xi.shape[1])
-            return np.dot(v.T, np.concatenate([np.ones(1), x], axis=0).reshape(-1,1)) + np.dot(w.T, (np.sqrt(np.diag(np.dot(S.T, S))) ** 3))
-#%%
+            S = xi - np.tile(x.reshape(-1, 1), xi.shape[1])
+            return np.dot(v.T, np.concatenate([np.ones(1), x], axis=0).reshape(-1, 1)) + np.dot(w.T, (
+            np.sqrt(np.diag(np.dot(S.T, S))) ** 3))
+
+
+# %%
 def interpolate_grad(x, inter_par):
     if inter_par.method == "NPS":
         w = inter_par.w
@@ -137,7 +148,7 @@ def interpolate_grad(x, inter_par):
         n = x.shape[0]
         N = xi.shape[1]
         g = np.zeros((n))
-        x1=np.copy(x)
+        x1 = np.copy(x)
         x = pd.DataFrame(x1).values
         for ii in range(N):
             X = x[:, 0] - xi[:, ii]
@@ -150,8 +161,11 @@ def interpolate_grad(x, inter_par):
         g = g + v[1:, 0]
 
         return g.T
-#%%
-import inspect,dis
+
+
+# %%
+import inspect, dis
+
 
 def expecting():
     """Return how many values the caller is expecting"""
@@ -160,63 +174,73 @@ def expecting():
     c = f.f_code
     i = f.f_lasti
     bytecode = c.co_code
-    instruction = bytecode[i+3]
+    instruction = bytecode[i + 3]
     if instruction == dis.opmap['UNPACK_SEQUENCE']:
-        howmany = bytecode[i+4]
+        howmany = bytecode[i + 4]
         return howmany
     elif instruction == dis.opmap['POP_TOP']:
         return 0
     return 1
-#%%
+
+
+# %%
 
 
 
 
 
 inter_par = Inter_par("NPS")
-xi = np.array([[0.5,0.8,0.2,0.6]])
-fun = lambda x:  np.multiply(x-0.45,x-0.45)
+xi = np.array([[0.5, 0.8, 0.2, 0.6]])
+fun = lambda x: np.multiply(x - 0.45, x - 0.45)
 yi = fun(xi)
 print(yi)
 K = 3
-inter_par = interpolateparameterization(xi,yi,inter_par)
+inter_par = interpolateparameterization(xi, yi, inter_par)
 ymin = np.min(yi)
 ind_min = np.argmin(yi)
-xm,ym = tringulation_search_bound_constantK(inter_par,xi,K,ind_min)
-#%%
+xm, ym = tringulation_search_bound_constantK(inter_par, xi, K, ind_min)
+
+
+# %%
 
 
 
-def fun(x,  alpha=0.1):
-    y = np.array((x[0,:]-0.45)**2.0 + alpha*(x[1,:]-0.45)**2.0)
+def fun(x, alpha=0.1):
+    y = np.array((x[0, :] - 0.45) ** 2.0 + alpha * (x[1, :] - 0.45) ** 2.0)
     return y.T
+
+
 #    return (x[0,:]-0.45)**2.0 + alpha*(x[1,:]-0.45)**2.0
 
 
 inter_par = Inter_par("NPS")
-xi = np.array([[0.5000 , 0.8000   , 0.5000,    0.2000,    0.5000],  [0.5000,    0.5000,    0.8000,    0.5000,    0.2000]])
-#xi=np.random.rand(2,3)
-#x=np.array([[0.5],[0.5]])
-#yi=np.random.rand(1,3)
-yi=fun(xi)
+xi = np.array([[0.5000, 0.8000, 0.5000, 0.2000, 0.5000], [0.5000, 0.5000, 0.8000, 0.5000, 0.2000]])
+# xi=np.random.rand(2,3)
+# x=np.array([[0.5],[0.5]])
+# yi=np.random.rand(1,3)
+yi = fun(xi)
 print (yi)
-#yi = np.array(yi)
-#print(yi.shape)
-#print(xi.shape)
+# yi = np.array(yi)
+# print(yi.shape)
+# print(xi.shape)
 #
 inter_par = interpolateparameterization(xi, yi, inter_par)
 
 ymin = np.min(yi)
 ind_min = np.argmin(yi)
-xm,ym = tringulation_search_bound_constantK(inter_par,xi,K,ind_min)
-#%%
-def test(x,y):
+xm, ym = tringulation_search_bound_constantK(inter_par, xi, K, ind_min)
+
+
+# %%
+def test(x, y):
     n_a = expecting()
     if n_a > 1:
-        return x+y,x-y
-    return x*y
-c1,s1 = test(11,2)
-print('c1',c1,'s1',s1)
-#ft = lambda x:test(x,2)
-#c,s = ft(11)
-#print(c)
+        return x + y, x - y
+    return x * y
+
+
+c1, s1 = test(11, 2)
+print('c1', c1, 's1', s1)
+# ft = lambda x:test(x,2)
+# c,s = ft(11)
+# print(c)
